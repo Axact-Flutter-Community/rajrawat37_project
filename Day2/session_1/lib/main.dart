9afe76e44640ce1b608eb2ad15a65c05a84438e7
rajrawat37 <rawatraj995@gmail.com>
2020-09-16 11:36:35 +0530

    layout done

--- Day2/session_1/lib/main.dart
+++ Day2/session_1/lib/main.dart
@@ -28,43 +28,49 @@ class _MyHomePageState extends State<MyHomePage> {
     return SafeArea(
       child: Row(
         children: <Widget>[
-            //mainAxisSize: MainAxisSize.min,
-            //verticalDirection: VerticalDirection.up,
-            //mainAxisAlignment: MainAxisAlignment.end,
-            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //crossAxisAlignment: CrossAxisAlignment.end,
-            //crossAxisAlignment: CrossAxisAlignment.stretch,
+          //mainAxisSize: MainAxisSize.min,
+          //verticalDirection: VerticalDirection.up,
+          //mainAxisAlignment: MainAxisAlignment.end,
+          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //crossAxisAlignment: CrossAxisAlignment.end,
+          //crossAxisAlignment: CrossAxisAlignment.stretch,
+          Container(
+            margin: EdgeInsets.only(top: 60, bottom: 50),
+            width: 100,
+            height: 600,
+            color: Colors.deepOrange,
+          ),
+          SizedBox(
+            width: 30,
+            height: 600,
+          ),
+          Column(
+            children: <Widget>[
               Container(
-                margin: EdgeInsets.only(top: 60, bottom: 50),
+                margin: EdgeInsets.only(top: 300),
+                color: Colors.yellow,
+                height: 100,
                 width: 100,
-                height: 600,
-                color: Colors.deepOrange,
               ),
-              SizedBox(width: 30,height: 600,),
-              Column(
-                 children:<Widget> [
-                    Container(
-                      margin: EdgeInsets.only(top: 300),
-                      color: Colors.yellow,
-                       height: 100,
-                       width: 100,
-                    ),
-                    Container(
-                      color: Colors.green,
-                       height: 100,
-                       width: 100,
-                    ),
-                 ],
-              ),
-              SizedBox(width: 30,height: 600,),
-                Container(
-                margin: EdgeInsets.only(top: 60, bottom: 50),
+              Container(
+                color: Colors.green,
+                height: 100,
                 width: 100,
-                height: 600,
-                color: Colors.blue,
               ),
             ],
+          ),
+          SizedBox(
+            width: 30,
+            height: 600,
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 60, bottom: 50),
+            width: 100,
+            height: 600,
+            color: Colors.blue,
+          ),
+        ],
       ),
     );
   }
